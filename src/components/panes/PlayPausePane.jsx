51751bdc135dfe64d14fe87ec28bc8ee2d1b03d5
Luca Aguzzoli <luca.aguzzoli@hotmail.it>
2016-09-13 16:15:34 +0200

    edit and fixed the topbar

--- src/components/panes/PlayPausePane.jsx
+++ src/components/panes/PlayPausePane.jsx
@@ -7,21 +7,21 @@ var PlayPausePane = React.createClass({
    _handlePrevButton() {
       this.props.onButtonClick('prev')
    },
-   _handlePrevButton() {
+   _handleNextButton() {
       this.props.onButtonClick('next')
    },
    render() {
       return (
-         <div className="playpausepane topbar-component left">
+         <div className="playpausepane">
             <div className="button-container">
                <div className="button-cell">
-                  <button className="rounded-button nav-button" onClick={this._handlePlayButton}></button>
+                  <button className="rounded-button nav-button" onClick={this._handlePrevButton}></button>
                </div>
                <div className="button-cell">
-                  <button className="rounded-button play-button" onClick={this._handleStopButton}></button>
+                  <button className="rounded-button play-button" onClick={this._handlePlayButton}></button>
                </div>
                <div className="button-cell">
-                  <button className="rounded-button nav-button" onClick={this._handlePauseButton}></button>
+                  <button className="rounded-button nav-button" onClick={this._handleNextButton}></button>
                </div>
             </div>
 
