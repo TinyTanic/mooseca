7072b5c6bf672614c62b2a6c3bc4f7a2c32169dc
Luca Aguzzoli <luca.aguzzoli@hotmail.it>
2016-09-20 12:25:19 +0200

    removed the frame border of the window and implemented the window button to close, minimize and maximize the window

--- src/components/panes/WindowButtonsPane.jsx
+++ src/components/panes/WindowButtonsPane.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
+const window = require('electron').remote.getCurrentWindow()
 
 var WindowButtonsPane = React.createClass({
+   _handleClickExit() {
+      window.close()
+   },
+   _handleClickMinimize() {
+      window.minimize()
+   },
+   _handleClickMaximize() {
+      if (!window.isMaximized()) {
+         window.maximize();
+      } else {
+         window.unmaximize();
+      }
+   },
    render() {
       return (
          <div className="window-buttons">
             <div className="buttons-container">
-               <button className="minimize"></button>
-               <button className="enlarge"></button>
-               <button className="close"></button>
+               <button className="minimize" onClick={this._handleClickMinimize}></button>
+               <button className="enlarge" onClick={this._handleClickMaximize}></button>
+               <button className="close" onClick={this._handleClickExit}></button>
             </div>
          </div>
       )
