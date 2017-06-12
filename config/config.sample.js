4fe0d0a38e74002fd0e0837fef4d4f1feb7240ba
Giuseppe Mammolo <gmammolo@gmail.com>
2017-06-12 16:49:20 +0200

    -rimosso file config da git

---
+++ config/config.sample.js
@@ -0,0 +1,9 @@
+/* FAX simile config.js */
+
+const conf = {
+  REDUX_DEV_TOOLS: `${require(
+    'os'
+  ).homedir()}/.config/google-chrome/Profile 5/Extensions/lmhkpmbekcpmknklioeibfkpmmfibljd/2.15.1_0`,
+}
+
+module.exports = conf
