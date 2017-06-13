c1f3c536f2f4e1e4edc7f0fb2d77830c97e94b37
Giuseppe Mammolo <gmammolo@gmail.com>
2017-06-13 11:10:45 +0200

    -configurazione personale

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@
 const conf = {
   REDUX_DEV_TOOLS: `${require(
     'os'
-  ).homedir()}/.config/chromium/Default/Extensions/lmhkpmbekcpmknklioeibfkpmmfibljd/2.15.1_0`,
+  ).homedir()}/.config/google-chrome/Profile 5/Extensions/lmhkpmbekcpmknklioeibfkpmmfibljd/2.15.1_0`,
 }
 
 module.exports = conf
